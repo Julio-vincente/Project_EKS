74d9b9e9c63551ec4165ebffd4f404ace46a30db
julio <juliomacedo.vicente@gmail.com>
2024-09-20 16:25:50 -0300

    test

--- project/modules/Cloud_Watch/main.tf
+++ project/modules/Cloud_Watch/main.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_metric_alarm" "alarm-julio" {
+resource "aws_cloudwatch_metric_alarm" "Prod-alarmCpu" {
   alarm_name          = var.metric_name
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
@@ -9,4 +9,10 @@ resource "aws_cloudwatch_metric_alarm" "alarm-julio" {
   threshold           = 80
   alarm_description   = "This metric monitors ec2 cpu utilization"
   insufficient_data_actions = []
+}
+
+
+resource "aws_cloudwatch_dashboard" "Prod-Dash" {
+  dashboard_name = 
+  
 }
--- project/modules/secrets_manager/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_secretsmanager_secret" "example" {
-  name = var.secret_name
-  description = var.description_secret_manager
-}
-
-resource "aws_secretsmanager_secret_version" "Version-SM" {
-  secret_id = aws_secretsmanager_secret.example.id
-  secret_string = var.User_Password 
-}
