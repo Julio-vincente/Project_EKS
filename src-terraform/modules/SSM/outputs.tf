56b8266c6b0497275dd6615444bb7b22bd2e2052
Julio <juliomacedo.vicente@gmail.com>
2024-09-26 15:11:53 -0300

    Delete src-terraform/modules/SSM directory

--- src-terraform/modules/SSM/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "rds_username" {
-  value = jsondecode(aws_secretsmanager_secret_version.rds_password_secret_version.secret_string)["username"]
-}
-
-output "rds_password" {
-  value = jsondecode(aws_secretsmanager_secret_version.rds_password_secret_version.secret_string)["password"]
-}
--- src-terraform/modules/SSM/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name_secret" {
-  type    = string
-  default = "rds_key"
-}
-
-variable "description_ssm" {
-  type    = string
-  default = "senha e usuario padrao do rds"
-}
