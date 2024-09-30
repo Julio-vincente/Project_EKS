2b9cb9935936ebab525df3911ec86ad8ab21a109
julio <juliomacedo.vicente@gmail.com>
2024-09-30 10:32:24 -0300

    att workflow

--- src-terraform/modules/S3/main.tf
+++ src-terraform/modules/S3/main.tf
@@ -15,9 +15,3 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-
-resource "aws_s3_object" "adicionando_pasta" {
-  bucket = aws_s3_bucket.bucket_rds.id
-  key    = var.pasta_s3
-  source = var.source_s3
-}
--- src-terraform/modules/S3/variables.tf
+++ src-terraform/modules/S3/variables.tf
@@ -2,13 +2,3 @@ variable "name_s3" {
   type    = string
   default = "julio344"
 }
-
-variable "pasta_s3" {
-  type = string
-  default = "OuPut/" 
-}
-
-variable "source_s3" {
-  type = string
-  default = "empty"
-}
